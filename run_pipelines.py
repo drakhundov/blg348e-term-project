95259d5c5e44a4b66cb98406617b9592aa964b23
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2024-12-24 23:34:18 +0300

    Put BAM files and the BED file into "cosap_data" folder. Then run docker using "docker run -it -v $(project folder)/cosap_data:/cosap_data -v $(project folder):/app itubioinformatics/cosap". The docker environment assumes all data is in /cosap_data, which is one of the problems. Another problem is that BAM files are unsorted and unindexed, which is done in the python script. Then, use "cd /app" (switch to the project folder within the docker environment). Run "python3 run_pipelines.py --process-bam". Enjoy!

--- run_pipelines.py
+++ run_pipelines.py
@@ -3,24 +3,21 @@ from cosap import (
     BamReader,
     Pipeline,
     PipelineRunner,
-    PipelineKeys,
     Recalibrator,
     VariantCaller,
 )
 import os, sys
-import yaml
 from datetime import datetime
-from typing import Dict
 import logging
 import subprocess
 
 TIMESTAMP = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
 
 # !LOGGING
-if not os.path.exists("logs"):
-    os.makedirs("logs")
+if not os.path.exists("log"):
+    os.makedirs("log")
 logging.basicConfig(
-    filename=f"logs/{TIMESTAMP}.log",
+    filename=f"log/{TIMESTAMP}.log",
     filemode="w",
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%H:%M:%S",
@@ -33,52 +30,13 @@ BAM_FILENAMES = {
     "bowtie": {"tumor": "tumor_bowtie.bam", "germline": "normal_bowtie.bam"},
     "bwa": {"tumor": "tumor_bwa.bam", "germline": "normal_bwa.bam"},
 }
-REF_GENOME = "data/Homo_sapiens_assembly38.fasta"
-KNOWN_SITES = [
-    "data/1000G_phase1.snps.high_confidence.hg38.vcf.gz",
-    "data/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
-]
+BED_FILE = "/cosap_data/High-Confidence_Regions_v1.2.bed"
 
 # !VCF
 VCF_DIR = "variants"
-
-# !CONFIGURATIONS
-CONF_DIR = "configurations"
-if not os.path.exists(CONF_DIR):
-    os.makedirs(CONF_DIR)
-logger.info(f"Configuration directory set to: {CONF_DIR}")
-
-# !CLEARUP
-# Directories and files to be cleared up (e.g., temp files, configurations).
-CLEARUP_LST = [
-    "tmp*",
-    "PREPROCESSOR",
-    "output*",
-    "workflow_dag.svg",
-    "*.yaml",
-    ".snakemake",
-]
-if "--clearup-logs" in sys.argv:
-    CLEARUP_LST.extend(
-        [
-            os.path.join("logs", path)
-            for path in os.listdir("logs")
-            if path != f"{TIMESTAMP}.log"
-        ]
-    )
-
-
-def clearup():
-    for path in CLEARUP_LST:
-        logger.info(f"Deleting {path}")
-        subprocess.run(
-            f"rm -rf {path}",
-            shell=True,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-
+if not os.path.exists(VCF_DIR):
+    os.makedirs(VCF_DIR)
+logger.info(f"Variants directory set to: {VCF_DIR}")
 
 # !
 # Kind of like C++ enum.
@@ -94,21 +52,11 @@ def get_bam_reader(mapper: str, state: int):
     else:
         raise Exception('File state not recognized. Use "GERMLINE" or "TUMOR"')
     logger.info(f"Getting BAM reader for {mapper} - {state}")
-    return BamReader(os.path.join("data", BAM_FILENAMES[mapper][state]))
-
-
-def create_config_file(conf_dir: str, config_data: Dict):
-    path = os.path.join(CONF_DIR, conf_dir, f"{TIMESTAMP}_config.yaml")
-    logger.info(f"Creating config file at {path}")
-    with open(path, "w") as config_file:
-        yaml.dump(config_data, config_file)
-    return path
+    return BamReader(os.path.join("/cosap_data", BAM_FILENAMES[mapper][state]))
 
 
 def create_pipeline(mapper: str, caller: str, use_recalibration: bool):
-    logger.info(
-        f"Creating pipeline: Mapper={mapper}, Caller={caller}, Recalibration={use_recalibration}"
-    )
+    logger.info("Creating pipeline")
     # Read BAM files.
     germline_bam = get_bam_reader(mapper=mapper, state=GERMLINE)
     tumor_bam = get_bam_reader(mapper=mapper, state=TUMOR)
@@ -117,40 +65,54 @@ def create_pipeline(mapper: str, caller: str, use_recalibration: bool):
 
     # Add duplicate removal.
     logger.info("Adding duplicate removal to pipeline")
-    mdup_normal = MDUP(input_step=germline_bam, spark=False)
-    mdup_tumor = MDUP(input_step=tumor_bam, spark=False)
+    mdup_normal = MDUP(input_step=germline_bam)
+    mdup_tumor = MDUP(input_step=tumor_bam)
     pipeline.add(mdup_normal)
     pipeline.add(mdup_tumor)
 
     if use_recalibration:
         # Add base recalibration.
         logger.info("Adding base recalibration to pipeline")
-        basecal_germline = Recalibrator(input_step=mdup_normal)
-        basecal_tumor = Recalibrator(input_step=mdup_tumor)
+        basecal_germline = Recalibrator(input_step=germline_bam, bed_file=BED_FILE)
+        basecal_tumor = Recalibrator(input_step=tumor_bam, bed_file=BED_FILE)
         pipeline.add(basecal_germline)
         pipeline.add(basecal_tumor)
         germline_input = basecal_germline
         tumor_input = basecal_tumor
     else:
-        germline_input = mdup_normal
-        tumor_input = mdup_tumor
-    # Add variant caller
+        germline_input = germline_bam
+        tumor_input = tumor_bam
+    # Add variant caller.
     logger.info("Adding variant caller to pipeline")
     caller = VariantCaller(
         library=caller,
         germline=germline_input,
         tumor=tumor_input,
+        bed_file=BED_FILE,
         params={
-            "reference": REF_GENOME,
             "output_vcf": os.path.join(
-                VCF_DIR, "{mapper}_{caller}_{'recal' if use_recalibration else ''}.vcf"
+                VCF_DIR, f"{mapper}_{caller}_{'recal' if use_recalibration else ''}.vcf"
             ),
             "threads": 1,  # Limit to single thread for stability
         },
     )
-
     pipeline.add(caller)
     return pipeline
+
+
+def sort_and_index_bam_files():
+    bam_dir = "/cosap_data"
+    for bam_file in os.listdir(bam_dir):
+        if bam_file.endswith(".bam"):
+            bam_path = os.path.join(bam_dir, bam_file)
+            sorted_bam_path = os.path.join(bam_dir, f"sorted_{bam_file}")
+            logger.info(f"Sorting {bam_path}")
+            subprocess.run(["samtools", "sort", bam_path, "-o", sorted_bam_path])
+            logger.info(f"Indexing {sorted_bam_path}")
+            subprocess.run(["samtools", "index", sorted_bam_path])
+            os.rename(sorted_bam_path, bam_path)
+            os.rename(f"{sorted_bam_path}.bai", f"{bam_path}.bai")
+            logger.info(f"Processed {bam_file}")
 
 
 def run_all_pipelines():
@@ -166,34 +128,26 @@ def run_all_pipelines():
             for use_recal in recalibration_options:
                 current_pipeline += 1
                 logger.debug(f"PIPELINE {current_pipeline}/{total_pipelines}")
-                logger.debug(
+                logger.info(
                     f"Configuration: Mapper={mapper}, Caller={caller}, Recalibration={use_recal}"
                 )
                 try:
                     pipeline = create_pipeline(mapper, caller, use_recal)
                     config = pipeline.build()
-                    # conf_dir = f"output_{mapper}_{caller}_{'recal' if use_recal else 'norecal'}"
-                    # os.makedirs(conf_dir, exist_ok=True)
-                    # config_file_path = create_config_file(conf_dir, config)
-                    # if not os.path.exists(config_file_path):
-                    #     logger.error(
-                    #         f"Could not create configuration file: {config_file_path}"
-                    #     )
-                    #     continue
-                    # config[PipelineKeys.WORKDIR] = os.path.dirname(config_file_path)
+                    logger.debug("Running pipeline.")
                     pipeline_runner = PipelineRunner()
                     pipeline_runner.run_pipeline(config)
                     logger.debug(
                         f"Successfully completed pipeline {current_pipeline}/{total_pipelines}"
                     )
                 except Exception as e:
-                    raise e
                     logger.error(
-                        f"Pipeline {current_pipeline}/{total_pipelines} failed: {str(e)}"
+                        f"Pipeline {current_pipeline}/{total_pipelines} failed: {e}"
                     )
                     continue
 
 
 if __name__ == "__main__":
-    clearup()
+    if "--process-bam" in sys.argv:
+        sort_and_index_bam_files()
     run_all_pipelines()
